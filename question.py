5d511447ba0d0fa1045c14bb1148b827f0a4cf57
Amit Raj <83224643+Amit4529@users.noreply.github.com>
2024-09-16 11:18:14 +0530

    question.py

--- question.py
+++ question.py
@@ -2,9 +2,19 @@ st=int(input("Enter starting point:-"))
 end=int(input("Enter ending point:-"))
 upd=int(input("Enter updation:-"))
 dir=input("Enter h for horizontal and v for vertical:- ")
+order=input("Enput f for forward order and r for reverse order")
 if(dir=='h'):
-    for i in range(st,end+1,upd):
-        print(i , end=" ")
-else:
-    for i in range(st,end,upd):
-        print(i)
+    if(order=='f'):
+        for i in range(st,end+1,upd):
+            print(i , end=" ")
+    else:
+        for i in range(end,st-1,-upd):
+            print(i , end=" ")
+
+elif(dir=='v'):
+    if(order=='f'):
+        for i in range(st,end+1,upd):
+            print(i)
+    else:
+        for i in range(end,st-1,-upd):
+            print(i)
